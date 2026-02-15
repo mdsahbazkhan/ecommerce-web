5d4d50da78bfeebe48a89c678bc516fc30d72369
sahbaz23 <amdsahbaz19@gmail.com>
2026-02-15 20:46:29 +0530

    backend fixed

--- backend/index.js
+++ backend/index.js
@@ -19,13 +19,12 @@ connectCloudinary();
 //Middlewares
 app.use(express.json());
 app.use(cors());
-
+// Root route
+app.get("/", (req, res) => {
+  res.send("Bazario Backend is running on Vercel 🚀");
+});
 // Api EndPoints
 app.use("/api/user", userRouter);
 app.use("/api/product", productRouter);
 app.use("/api/cart", cartRouter);
 app.use("/api/order", orderRouter);
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
