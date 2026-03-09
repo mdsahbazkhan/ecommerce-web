75faff5541c7c7a9bb5074d9518548a77d7485a3
sahbaz23 <amdsahbaz19@gmail.com>
2026-03-09 14:34:19 +0530

    fix scroll top f

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,33 +19,37 @@ import ShippingDelivery from "./pages/ShippingDelivery";
 import ReturnsExchanges from "./pages/ReturnsExchanges";
 import Verify from "./pages/Verify";
 import Profile from "./pages/Profile";
+import ScrollToTop from "./components/ScrollToTop";
 
 const App = () => {
   return (
-    <div className="px-4 sm:px-[5vw] md:px-[5vw] lg:px-[5vw]">
-      <Navbar />
-      <SearchBar />
-      <Toaster richColors position="top-right" />
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/collection" element={<Collection />}></Route>
-        <Route path="/about" element={<About />}></Route>
-        <Route path="/product/:productId" element={<Product />}></Route>
-        <Route path="/cart" element={<Cart />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="/place-order" element={<PlaceOrder />}></Route>
-        <Route path="/profile" element={<Profile />}></Route>
-        <Route path="/orders" element={<Orders />}></Route>{" "}
-        <Route path="/verify" element={<Verify />}></Route>
-        <Route path="/faq" element={<FAQ />}></Route>
-        <Route path="/privacy" element={<PrivacyPolicy />} />
-        <Route path="/terms" element={<TermsConditions />} />
-        <Route path="/shipping" element={<ShippingDelivery />} />
-        <Route path="/returns" element={<ReturnsExchanges />} />
-        <Route path="/support" element={<Contact />}></Route>
-      </Routes>
-      <Footer />
-    </div>
+    <>
+      <ScrollToTop />
+      <div className="px-4 sm:px-[5vw] md:px-[5vw] lg:px-[5vw]">
+        <Navbar />
+        <SearchBar />
+        <Toaster richColors position="top-right" />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/collection" element={<Collection />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/product/:productId" element={<Product />}></Route>
+          <Route path="/cart" element={<Cart />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/place-order" element={<PlaceOrder />}></Route>
+          <Route path="/profile" element={<Profile />}></Route>
+          <Route path="/orders" element={<Orders />}></Route>{" "}
+          <Route path="/verify" element={<Verify />}></Route>
+          <Route path="/faq" element={<FAQ />}></Route>
+          <Route path="/privacy" element={<PrivacyPolicy />} />
+          <Route path="/terms" element={<TermsConditions />} />
+          <Route path="/shipping" element={<ShippingDelivery />} />
+          <Route path="/returns" element={<ReturnsExchanges />} />
+          <Route path="/support" element={<Contact />}></Route>
+        </Routes>
+        <Footer />
+      </div>
+    </>
   );
 };
 
